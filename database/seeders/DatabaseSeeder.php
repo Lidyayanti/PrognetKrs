5eed2de4be0da44c32846dd749e5db186b0d1437
Fatliana <Fatliana29@gmail.com>
2021-12-04 18:23:17 +0800

    update file databaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,17 +25,201 @@ class DatabaseSeeder extends Seeder
     {
         Matakuliah::insert([
             [
-                'kode' => 'TI-TBD-1',
-                'nama_matakuliah' => 'TBD',
+                'kode' => 'TI-ALG-1',
+                'nama_matakuliah' => 'Algoritma',
+                'semester' => 1,
+                'sks' => 2,
+                'prodi' => DatabaseSeeder::$prodi[0],
+                'status_mk' => 'wajib'
+            ],
+            [
+                'kode' => 'TI-KBD-1',
+                'nama_matakuliah' => 'Konsep Basis Data',
+                'semester' => 1,
+                'sks' => 3,
+                'prodi' => DatabaseSeeder::$prodi[0],
+                'status_mk' => 'wajib'
+            ],
+            [
+                'kode' => 'TI-PMROG-1',
+                'nama_matakuliah' => 'Pemrograman',
+                'semester' => 1,
+                'sks' => 3,
+                'prodi' => DatabaseSeeder::$prodi[0],
+                'status_mk' => 'wajib'
+            ],
+            [
+                'kode' => 'TI-TBD-2',
+                'nama_matakuliah' => 'Teknologi Basis Data',
                 'semester' => 2,
                 'sks' => 3,
                 'prodi' => DatabaseSeeder::$prodi[0],
                 'status_mk' => 'wajib'
             ],
             [
-                'kode' => 'TI-KBD-1',
-                'nama_matakuliah' => 'KBD',
-                'semester' => 1,
+                'kode' => 'TI-JARKOM-2',
+                'nama_matakuliah' => 'Jaringan Komputer dan Komunikas',
+                'semester' => 2,
+                'sks' => 3,
+                'prodi' => DatabaseSeeder::$prodi[0],
+                'status_mk' => 'wajib'
+            ],
+            [
+                'kode' => 'TI-PBO-2',
+                'nama_matakuliah' => 'Pemrograman Berorientasi Obyek',
+                'semester' => 2,
+                'sks' => 3,
+                'prodi' => DatabaseSeeder::$prodi[0],
+                'status_mk' => 'wajib'
+            ],
+            [
+                'kode' => 'TI-PROGNET-3',
+                'nama_matakuliah' => 'Pemrograman internet',
+                'semester' => 3,
+                'sks' => 3,
+                'prodi' => DatabaseSeeder::$prodi[0],
+                'status_mk' => 'wajib'
+            ],
+            [
+                'kode' => 'TI-RPL-3',
+                'nama_matakuliah' => 'Rekayasa Perangkat Lunak',
+                'semester' => 3,
+                'sks' => 3,
+                'prodi' => DatabaseSeeder::$prodi[0],
+                'status_mk' => 'wajib'
+            ],
+            [
+                'kode' => 'TI-IMK-3',
+                'nama_matakuliah' => 'Interaksi Manusia Komputer',
+                'semester' => 3,
+                'sks' => 3,
+                'prodi' => DatabaseSeeder::$prodi[0],
+                'status_mk' => 'wajib'
+            ],
+            [
+                'kode' => 'TI-KT-4',
+                'nama_matakuliah' => 'Kecerdasan Tiruan',
+                'semester' => 4,
+                'sks' => 3,
+                'prodi' => DatabaseSeeder::$prodi[0],
+                'status_mk' => 'wajib'
+            ],
+            [
+                'kode' => 'TI-PROGMOB-4',
+                'nama_matakuliah' => 'Pemrograman mobile',
+                'semester' => 4,
+                'sks' => 3,
+                'prodi' => DatabaseSeeder::$prodi[0],
+                'status_mk' => 'wajib'
+            ],
+            [
+                'kode' => 'TI-IOT-4',
+                'nama_matakuliah' => 'Internet of Things',
+                'semester' => 4,
+                'sks' => 3,
+                'prodi' => DatabaseSeeder::$prodi[0],
+                'status_mk' => 'wajib'
+            ],
+            [
+                'kode' => 'TI-ITFOR-5',
+                'nama_matakuliah' => 'IT Forensic',
+                'semester' => 5,
+                'sks' => 3,
+                'prodi' => DatabaseSeeder::$prodi[0],
+                'status_mk' => 'wajib'
+            ],
+            [
+                'kode' => 'TI-AKB-5',
+                'nama_matakuliah' => 'Analisis Kelayakan Bisnis',
+                'semester' => 5,
+                'sks' => 3,
+                'prodi' => DatabaseSeeder::$prodi[0],
+                'status_mk' => 'wajib'
+            ],
+            [
+                'kode' => 'TI-ERP-5',
+                'nama_matakuliah' => 'Enterprise Resource Planning',
+                'semester' => 5,
+                'sks' => 3,
+                'prodi' => DatabaseSeeder::$prodi[0],
+                'status_mk' => 'wajib'
+            ],
+            [
+                'kode' => 'TI-IMP-6',
+                'nama_matakuliah' => 'Inovasi dan Manajemen Produk',
+                'semester' => 6,
+                'sks' => 3,
+                'prodi' => DatabaseSeeder::$prodi[0],
+                'status_mk' => 'wajib'
+            ],
+            [
+                'kode' => 'TI-IMP-6',
+                'nama_matakuliah' => 'Inovasi dan Manajemen Produk',
+                'semester' => 6,
+                'sks' => 3,
+                'prodi' => DatabaseSeeder::$prodi[0],
+                'status_mk' => 'wajib'
+            ],
+            [
+                'kode' => 'TI-IM-6',
+                'nama_matakuliah' => 'Internet Marketing',
+                'semester' => 6,
+                'sks' => 3,
+                'prodi' => DatabaseSeeder::$prodi[0],
+                'status_mk' => 'wajib'
+            ],
+            [
+                'kode' => 'TI-PT-6',
+                'nama_matakuliah' => 'Project Technopreneurship',
+                'semester' => 6,
+                'sks' => 3,
+                'prodi' => DatabaseSeeder::$prodi[0],
+                'status_mk' => 'wajib'
+            ],
+            [
+                'kode' => 'TI-PRMB-7',
+                'nama_matakuliah' => 'Projek Riset Manajemen Bisnis',
+                'semester' => 7,
+                'sks' => 3,
+                'prodi' => DatabaseSeeder::$prodi[0],
+                'status_mk' => 'wajib'
+            ],
+            [
+                'kode' => 'TI-SMB-7',
+                'nama_matakuliah' => 'Seminar Manajemen Bisnis',
+                'semester' => 7,
+                'sks' => 3,
+                'prodi' => DatabaseSeeder::$prodi[0],
+                'status_mk' => 'wajib'
+            ],
+            [
+                'kode' => 'TI-P1-7',
+                'nama_matakuliah' => 'Pilihan 1',
+                'semester' => 7,
+                'sks' => 3,
+                'prodi' => DatabaseSeeder::$prodi[0],
+                'status_mk' => 'wajib'
+            ],
+            [
+                'kode' => 'TI-KKN-8',
+                'nama_matakuliah' => 'Kuliah Kerja Nyata',
+                'semester' => 8,
+                'sks' => 3,
+                'prodi' => DatabaseSeeder::$prodi[0],
+                'status_mk' => 'wajib'
+            ],
+            [
+                'kode' => 'TI-PI-8',
+                'nama_matakuliah' => 'Penulisan Ilmiah ',
+                'semester' => 8,
+                'sks' => 3,
+                'prodi' => DatabaseSeeder::$prodi[0],
+                'status_mk' => 'wajib'
+            ],
+            [
+                'kode' => 'TI-TK-8',
+                'nama_matakuliah' => 'Tugas Akhir',
+                'semester' => 8,
                 'sks' => 3,
                 'prodi' => DatabaseSeeder::$prodi[0],
                 'status_mk' => 'wajib'
